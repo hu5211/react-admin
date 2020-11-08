8b49c81b086160d85c6c971d69ee5041243b01b9
hu5211 <635022034@qq.com>
2020-11-08 08:34:10 +0800

    init

---
+++ src/views/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class About extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return <div>About</div>
+    }
+}
+
+export default About;
